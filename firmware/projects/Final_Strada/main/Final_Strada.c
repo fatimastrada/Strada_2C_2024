255f958ffcf21d3399623eadb7269455e2374b8a
fatima <stradafatima621@gmail.com>
2024-12-03 10:09:33 -0300

    Se crea la carpeta Final_Strada

---
+++ firmware/projects/Final_Strada/main/Final_Strada.c
@@ -0,0 +1,39 @@
+/*! @mainpage Final Strada
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 03/12/2024 | Document creation		                         |
+ *
+ * @author Strada Fátima Anabel (fatima.strada@ingenieria.uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	
+}
+/*==================[end of file]============================================*/
